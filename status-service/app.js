34c2e8b057c173ebfb842cf3c4fc24f7b4f0dc09
Lamarkes <lamark12ricarte@gmail.com>
2026-05-29 14:46:52 -0300

    :sparkles: feat: serviço de status

---
+++ status-service/app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+process.env.TZ = 'America/Sao_Paulo';
+
+const hostname = process.env.HOSTNAME || "container-1";
+
+app.get("/", (req, res) => {
+    res.send("<h1> Hello from Status Server</h1>")
+});
+
+app.get("/status", (req, res) => {
+    const date_time = new Date();
+    const uptime = Math.floor(process.uptime());
+
+    const hours = Math.floor(uptime / 3600);
+    const minutes = Math.floor((uptime % 3600) / 60);
+    const seconds = uptime % 60;
+
+
+    res.json({
+        "status": "Online",
+        "uptime": `${hours} hrs:${minutes} min:${seconds} sec`,
+        "hostname": hostname
+    });
+    sendLogs(date_time.toString());
+});
+
+
+
+async function sendLogs(date_time) {
+
+    const response = { 
+        'service': 'status-service', 
+        "message": "GET /status executado",
+        "route": "/status",
+        'date': date_time,
+        "method": "GET",
+        "hostname": hostname
+    
+    }
+    try {
+        const resposta = await fetch('http://logservice:3003/log', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(response)
+        });
+    } catch (erro){
+        console.error('Ocorreu um erro:', erro);
+    }
+    
+}
+
+
+const PORT = process.env.PORT || 3001;
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}`)
+})
