99b3a614693bea5b7215d3cf405ba28f873c89de
Lamarkes <lamark12ricarte@gmail.com>
2026-06-01 22:56:58 -0300

    :sparkles: feat: adicionando logs no banco postgres

--- log-service/app.js
+++ log-service/app.js
@@ -1,24 +1,71 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-const logs = []
+const pg = require('pg');
 
+const { Pool } = pg
 
-app.post('/log', (req, res) => {
-
-    logs.push(req.body);
-
-    res.json({message:'Log criado!'});
+const pool = new Pool({
+    user: "admin",
+    host: "postgres",
+    database: "mydb",
+    password: "password",
 });
 
-app.get('/logs', (req, res) => {
-    res.json(logs);
+const queryCreateTable = `CREATE TABLE IF NOT EXISTS saved_logs (
+                            id SERIAL PRIMARY KEY,
+                            service_name VARCHAR(100) NOT NULL,
+                            message VARCHAR(100) NOT NULL,
+                            route VARCHAR(100) NOT NULL,
+                            method VARCHAR(30) NOT NULL,
+                            hostname VARCHAR(100) NOT NULL,
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`
+
+async function createTableFunction() {
+
+    try {
+        await pool.query(queryCreateTable);
+    } catch (error) {
+        console.log("Error to create table", error.stack)
+    }
+}
+
+const queryInsertTable = `INSERT INTO saved_logs(service_name, message, route, method, hostname)
+                            VALUES($1, $2, $3, $4, $5)`
+
+async function insertTableFunction(query, values) {
+    try {
+        const res = await pool.query(query, values);
+    } catch (error) {
+        console.log("Error to insert table", error.stack);
+    }
+}
+
+createTableFunction();
+app.post('/log', (req, res) => {
+    const values = [
+        req.body.service,
+        req.body.message,
+        req.body.route,
+        req.body.method,
+        req.body.hostname
+    ];
+    insertTableFunction(queryInsertTable, values);
+
+    res.json({ message: 'Log criado!' });
+});
+
+app.get('/logs', async (req, res) => {
+
+    const response = await pool.query("SELECT * FROM saved_logs");
+
+    res.json(response.rows);
 });
 
 
 const PORT = process.env.PORT || 3003;
 
 
-app.listen(PORT, ()=>{
-     console.log(`Running PORT: ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Running PORT: ${PORT}`);
 })
